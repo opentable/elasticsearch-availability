177a00e0b279ae7d9d6b65dcecf31769f37ea70c
Max Gurewitz <maxthegeek1@gmail.com>
2018-05-15 17:23:58 -0700

    logic for parsing slot

--- test/parseAvailabilityByteString.spec.js
+++ test/parseAvailabilityByteString.spec.js
@@ -2,10 +2,14 @@ const test = require('ava');
 const moment = require('moment');
 const parseAvailabilityByteString = require('../src/parseAvailabilityByteString');
 
-const sampleByteString = '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x80\x80\x80\x80\x80\x80\x00\x00\x00\x00\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x00\x00';
+const sampleByteString = '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\b\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88\x88';
 
 test('can parse availability status from byte string', t => {
   const dateTime = moment('2018-06-01', 'YYYY-MM-DD');
+  dateTime.add(18, 'hours');
+  dateTime.add(30, 'minutes');
+
   const parsed = parseAvailabilityByteString(dateTime, sampleByteString);
-  t.is(parsed.isAvailable, true, 'slot has availability');
+
+  t.is(parsed[0].isAvailable, true, 'slot has availability');
 });
